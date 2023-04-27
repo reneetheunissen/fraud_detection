62da34975348c2db8106d6fb5006af8e7cfd9ea6
Renee <reneetheunissen@me.com>
2023-04-27 16:07:02 +0200

    Added Random Forest graphs

--- scenarios/RQ2/RQ2_metrics_per_observed_fraud_rate.py
+++ scenarios/RQ2/RQ2_metrics_per_observed_fraud_rate.py
@@ -1,20 +1,19 @@
-
-# Get graph of the metrics
 from information_and_metrics import MetricsPerObservedFraudRate
 
+# Get graph of the metrics
 metrics_over_time = MetricsPerObservedFraudRate()
 # TPR group
 metrics_over_time.plot_metrics_by_observed_fraud(
     metrics_to_use='TPR_GROUP',
-    classifier_name='LogisticRegression'
+    classifier_name='RandomForest'
 )
 # FDR group
 metrics_over_time.plot_metrics_by_observed_fraud(
     metrics_to_use='FDR_GROUP',
-    classifier_name='LogisticRegression'
+    classifier_name='RandomForest'
 )
 # RPP group
 metrics_over_time.plot_metrics_by_observed_fraud(
     metrics_to_use='RPP_GROUP',
-    classifier_name='LogisticRegression'
+    classifier_name='RandomForest'
 )
