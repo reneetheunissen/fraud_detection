1f3c70c548aa36cc750d925c3eb4e760e7aa937b
Renee <reneetheunissen@me.com>
2023-05-09 21:52:27 +0200

    Fix: improved plots

--- scenarios/RQ3_metrics_over_time.py
+++ scenarios/RQ3_metrics_over_time.py
@@ -1,35 +1,35 @@
 from fraud_detector import TransactionsOverTime
 
-print("Random Sampling")
-bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.2,
-    female_fraud_proportion=0.1,
-    classifier_name='RandomForest',
-    sample_size=6500,
-    random_training_set=True,
-    amount_of_days=10,
-    title_scenario="Random Sampling",
-    percentage_alerts=0.1,
-)
-bank_process.plot(n_iterations=10)
-#
-# print()
-#
-# print("Selection bias scenario 2")
+# print("Random Sampling")
 # bank_process: TransactionsOverTime = TransactionsOverTime(
-#     male_fraud_proportion=0.3,
+#     male_fraud_proportion=0.2,
 #     female_fraud_proportion=0.1,
 #     classifier_name='RandomForest',
 #     sample_size=6500,
-#     amount_of_days=50,
-#     title_scenario="Over-representation",
+#     random_training_set=True,
+#     amount_of_days=25,
+#     title_scenario="Random Sampling",
 #     percentage_alerts=0.1,
-#     active_learning=True,
-#     percentage_active_learning=0.1,
 # )
-# bank_process.plot(n_iterations=100)
+# bank_process.plot(n_iterations=25)
 #
 # print()
+
+print("Selection bias scenario 2")
+bank_process: TransactionsOverTime = TransactionsOverTime(
+    male_fraud_proportion=0.3,
+    female_fraud_proportion=0.1,
+    classifier_name='RandomForest',
+    sample_size=6500,
+    amount_of_days=25,
+    title_scenario="Over-representation",
+    percentage_alerts=0.1,
+    active_learning=True,
+    percentage_active_learning=0.1,
+)
+bank_process.plot(n_iterations=25)
+
+print()
 
 # print("Selection bias scenario 3")
 # bank_process: TransactionsOverTime = TransactionsOverTime(
