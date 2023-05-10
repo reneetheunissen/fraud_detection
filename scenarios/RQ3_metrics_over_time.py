8658d215728c0b5f2cb34862a7829f1f5c16920f
Renee <reneetheunissen@me.com>
2023-05-10 14:33:33 +0200

    Removed unnecessary print statements

--- scenarios/RQ3_metrics_over_time.py
+++ scenarios/RQ3_metrics_over_time.py
@@ -1,19 +1,19 @@
 from fraud_detector import TransactionsOverTime
 
-print("Random Sampling")
-bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.2,
-    female_fraud_proportion=0.1,
-    classifier_name='LogisticRegression',
-    sample_size=6500,
-    random_training_set=True,
-    amount_of_days=25,
-    title_scenario="Random Sampling",
-    percentage_alerts=0.1,
-)
-bank_process.plot(n_iterations=25)
-
-print()
+# print("Random Sampling")
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.2,
+#     female_fraud_proportion=0.1,
+#     classifier_name='LogisticRegression',
+#     sample_size=6500,
+#     random_training_set=True,
+#     amount_of_days=25,
+#     title_scenario="Random Sampling",
+#     percentage_alerts=0.1,
+# )
+# bank_process.plot(n_iterations=25)
+#
+# print()
 
 print("Selection bias scenario 2")
 bank_process: TransactionsOverTime = TransactionsOverTime(
