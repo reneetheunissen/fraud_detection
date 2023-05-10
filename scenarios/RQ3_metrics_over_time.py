d043dc252472b40c691d7bbbb7bf3452afc38aff
Renee <reneetheunissen@me.com>
2023-05-10 20:33:35 +0200

    Renewed LR FPR plots

--- scenarios/RQ3_metrics_over_time.py
+++ scenarios/RQ3_metrics_over_time.py
@@ -2,27 +2,43 @@ from fraud_detector import TransactionsOverTime
 
 print("Random Sampling")
 bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.2,
-    female_fraud_proportion=0.1,
+    male_fraud_proportion=0.3,
+    female_fraud_proportion=0.05,
     classifier_name='LogisticRegression',
     sample_size=6500,
-    random_training_set=True,
+    # random_training_set=True,
     amount_of_days=25,
-    title_scenario="Random Sampling",
+    title_scenario="Biased Data",
     percentage_alerts=0.1,
 )
 bank_process.plot(n_iterations=25)
 
 print()
 
-print("Selection bias scenario 2")
+print("Random Sampling")
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
-    female_fraud_proportion=0.1,
+    female_fraud_proportion=0.05,
     classifier_name='LogisticRegression',
     sample_size=6500,
+    # random_training_set=True,
     amount_of_days=25,
-    title_scenario="Over-representation",
-    percentage_alerts=0.1,
+    title_scenario="Biased Data",
+    percentage_alerts=0.05,
+)
+bank_process.plot(n_iterations=25)
+
+print()
+
+print("Random Sampling")
+bank_process: TransactionsOverTime = TransactionsOverTime(
+    male_fraud_proportion=0.3,
+    female_fraud_proportion=0.05,
+    classifier_name='LogisticRegression',
+    sample_size=6500,
+    # random_training_set=True,
+    amount_of_days=25,
+    title_scenario="Biased Data",
+    percentage_alerts=0.01,
 )
 bank_process.plot(n_iterations=25)
