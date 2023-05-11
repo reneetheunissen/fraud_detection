ea2c45b023eef7904be9a8423018e27dcc0b8a74
Renee <reneetheunissen@me.com>
2023-05-11 16:32:44 +0200

    added plots of 50 days 50 iterations RQ3

--- scenarios/RQ3_metrics_over_time.py
+++ scenarios/RQ3_metrics_over_time.py
@@ -3,39 +3,23 @@ from fraud_detector import TransactionsOverTime
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
     female_fraud_proportion=0.05,
-    classifier_name='RandomForest',
+    classifier_name='LogisticRegression',
     sample_size=6500,
     # random_training_set=True,
     amount_of_days=25,
-    title_scenario="Biased Data, Random Forest",
-    percentage_alerts=0.1,
-)
-bank_process.plot(n_iterations=25)
-
-print()
-
-bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.3,
-    female_fraud_proportion=0.05,
-    classifier_name='RandomForest',
-    sample_size=6500,
-    # random_training_set=True,
-    amount_of_days=25,
-    title_scenario="Biased Data, Random Forest",
+    title_scenario="Biased Data, Logistic Regression",
     percentage_alerts=0.05,
 )
 bank_process.plot(n_iterations=25)
 
-print()
-
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
     female_fraud_proportion=0.05,
-    classifier_name='RandomForest',
+    classifier_name='LogisticRegression',
     sample_size=6500,
     # random_training_set=True,
     amount_of_days=25,
-    title_scenario="Biased Data, Random Forest",
+    title_scenario="Biased Data, Logistic Regression",
     percentage_alerts=0.01,
 )
 bank_process.plot(n_iterations=25)
