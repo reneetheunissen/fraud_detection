14d2130f69530cab14d3d7f015e3415d415ece96
Renee <reneetheunissen@me.com>
2023-05-12 00:07:38 +0200

    Added missing plot

--- scenarios/RQ3_metrics_over_time.py
+++ scenarios/RQ3_metrics_over_time.py
@@ -5,53 +5,11 @@ percentage_alerts = 0.1
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
     female_fraud_proportion=0.05,
-    classifier_name='LogisticRegression',
-    sample_size=6500,
-    amount_of_days=25,
-    title_scenario="Biased, LR, uncertainty, 10% exploration",
-    percentage_alerts=percentage_alerts,
-    active_learning=True,
-    percentage_active_learning=0.1,
-    al_type_name='uncertainty',
-)
-bank_process.plot(n_iterations=25)
-
-bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.3,
-    female_fraud_proportion=0.05,
-    classifier_name='LogisticRegression',
-    sample_size=6500,
-    amount_of_days=25,
-    title_scenario="Biased, LR, uncertainty, 25% exploration",
-    percentage_alerts=percentage_alerts,
-    active_learning=True,
-    percentage_active_learning=0.25,
-    al_type_name='uncertainty',
-)
-bank_process.plot(n_iterations=25)
-
-bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.3,
-    female_fraud_proportion=0.05,
-    classifier_name='LogisticRegression',
-    sample_size=6500,
-    amount_of_days=25,
-    title_scenario="Biased, LR, uncertainty, 50% exploration",
-    percentage_alerts=percentage_alerts,
-    active_learning=True,
-    percentage_active_learning=0.5,
-    al_type_name='uncertainty',
-)
-bank_process.plot(n_iterations=25)
-
-bank_process: TransactionsOverTime = TransactionsOverTime(
-    male_fraud_proportion=0.3,
-    female_fraud_proportion=0.05,
     classifier_name='RandomForest',
     sample_size=6500,
     amount_of_days=25,
     title_scenario="Biased, RF, uncertainty, 10% exploration",
-    percentage_alerts=percentage_alerts,
+    percentage_alerts=0.10,
     active_learning=True,
     percentage_active_learning=0.1,
     al_type_name='uncertainty',
@@ -61,27 +19,111 @@ bank_process.plot(n_iterations=25)
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
     female_fraud_proportion=0.05,
-    classifier_name='RandomForest',
+    classifier_name='LogisticRegression',
     sample_size=6500,
-    amount_of_days=25,
-    title_scenario="Biased, RF, uncertainty, 25% exploration",
-    percentage_alerts=percentage_alerts,
-    active_learning=True,
-    percentage_active_learning=0.25,
+    amount_of_days=50,
+    title_scenario="Biased, LR, uncertainty",
+    percentage_alerts=0.05,
+    active_learning=False,
+    percentage_active_learning=0.1,
     al_type_name='uncertainty',
 )
-bank_process.plot(n_iterations=25)
+bank_process.plot(n_iterations=50)
 
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
     female_fraud_proportion=0.05,
-    classifier_name='RandomForest',
+    classifier_name='LogisticRegression',
     sample_size=6500,
-    amount_of_days=25,
-    title_scenario="Biased, RF, uncertainty, 50% exploration",
-    percentage_alerts=percentage_alerts,
-    active_learning=True,
-    percentage_active_learning=0.5,
+    amount_of_days=50,
+    title_scenario="Biased, LR, uncertainty",
+    percentage_alerts=0.01,
+    active_learning=False,
+    percentage_active_learning=0.1,
     al_type_name='uncertainty',
 )
-bank_process.plot(n_iterations=25)
+bank_process.plot(n_iterations=50)
+
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.3,
+#     female_fraud_proportion=0.05,
+#     classifier_name='LogisticRegression',
+#     sample_size=6500,
+#     amount_of_days=25,
+#     title_scenario="Biased, LR, uncertainty, 10% exploration",
+#     percentage_alerts=percentage_alerts,
+#     active_learning=True,
+#     percentage_active_learning=0.1,
+#     al_type_name='uncertainty',
+# )
+# bank_process.plot(n_iterations=25)
+#
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.3,
+#     female_fraud_proportion=0.05,
+#     classifier_name='LogisticRegression',
+#     sample_size=6500,
+#     amount_of_days=25,
+#     title_scenario="Biased, LR, uncertainty, 25% exploration",
+#     percentage_alerts=percentage_alerts,
+#     active_learning=True,
+#     percentage_active_learning=0.25,
+#     al_type_name='uncertainty',
+# )
+# bank_process.plot(n_iterations=25)
+#
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.3,
+#     female_fraud_proportion=0.05,
+#     classifier_name='LogisticRegression',
+#     sample_size=6500,
+#     amount_of_days=25,
+#     title_scenario="Biased, LR, uncertainty, 50% exploration",
+#     percentage_alerts=percentage_alerts,
+#     active_learning=True,
+#     percentage_active_learning=0.5,
+#     al_type_name='uncertainty',
+# )
+# bank_process.plot(n_iterations=25)
+#
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.3,
+#     female_fraud_proportion=0.05,
+#     classifier_name='RandomForest',
+#     sample_size=6500,
+#     amount_of_days=25,
+#     title_scenario="Biased, RF, uncertainty, 10% exploration",
+#     percentage_alerts=percentage_alerts,
+#     active_learning=True,
+#     percentage_active_learning=0.1,
+#     al_type_name='uncertainty',
+# )
+# bank_process.plot(n_iterations=25)
+#
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.3,
+#     female_fraud_proportion=0.05,
+#     classifier_name='RandomForest',
+#     sample_size=6500,
+#     amount_of_days=25,
+#     title_scenario="Biased, RF, uncertainty, 25% exploration",
+#     percentage_alerts=percentage_alerts,
+#     active_learning=True,
+#     percentage_active_learning=0.25,
+#     al_type_name='uncertainty',
+# )
+# bank_process.plot(n_iterations=25)
+#
+# bank_process: TransactionsOverTime = TransactionsOverTime(
+#     male_fraud_proportion=0.3,
+#     female_fraud_proportion=0.05,
+#     classifier_name='RandomForest',
+#     sample_size=6500,
+#     amount_of_days=25,
+#     title_scenario="Biased, RF, uncertainty, 50% exploration",
+#     percentage_alerts=percentage_alerts,
+#     active_learning=True,
+#     percentage_active_learning=0.5,
+#     al_type_name='uncertainty',
+# )
+# bank_process.plot(n_iterations=25)
