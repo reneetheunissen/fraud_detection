de19db8fd8121b7578c6b974ef0a1c19d4d53510
Renee <reneetheunissen@me.com>
2023-05-12 20:55:39 +0200

    Added 100 days plots

--- scenarios/RQ3_metrics_over_time.py
+++ scenarios/RQ3_metrics_over_time.py
@@ -5,21 +5,19 @@ bank_process: TransactionsOverTime = TransactionsOverTime(
     female_fraud_proportion=0.05,
     classifier_name='LogisticRegression',
     sample_size=6500,
-    # random_training_set=True,
-    amount_of_days=25,
-    title_scenario="Biased Data, Logistic Regression",
+    amount_of_days=100,
+    title_scenario="Biased, LR, uncertainty",
     percentage_alerts=0.05,
 )
-bank_process.plot(n_iterations=25)
+bank_process.plot(n_iterations=100)
 
 bank_process: TransactionsOverTime = TransactionsOverTime(
     male_fraud_proportion=0.3,
     female_fraud_proportion=0.05,
     classifier_name='LogisticRegression',
     sample_size=6500,
-    # random_training_set=True,
-    amount_of_days=25,
-    title_scenario="Biased Data, Logistic Regression",
+    amount_of_days=100,
+    title_scenario="Biased, LR, uncertainty",
     percentage_alerts=0.01,
 )
-bank_process.plot(n_iterations=25)
+bank_process.plot(n_iterations=100)
