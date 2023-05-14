b05fce49b9df81fa1b43c83a0688d0bb5438421c
Renee <reneetheunissen@me.com>
2023-05-14 19:32:12 +0200

    Added titles to plots RQ2

--- scenarios/RQ2/RQ2_metrics_per_observed_fraud_rate.py
+++ scenarios/RQ2/RQ2_metrics_per_observed_fraud_rate.py
@@ -1,7 +1,13 @@
 from information_and_metrics import MetricsPerObservedFraudRate
 
-# Get graph of the metrics
 metrics_over_time = MetricsPerObservedFraudRate()
+
 metrics_over_time.plot_metrics_by_observed_fraud(
-    classifier_name='RandomForest'
+    classifier_name='LogisticRegression',
+    plot_title='Logistic Regression',
+)
+
+metrics_over_time.plot_metrics_by_observed_fraud(
+    classifier_name='RandomForest',
+    plot_title='Random Forest'
 )
